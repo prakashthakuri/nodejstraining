faed39d7b1f83208d725a250b1f4ed378fac6572
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-07-10 21:54:22 -0400

    MongoDB Connection

--- server.js
+++ server.js
@@ -8,22 +8,41 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+var mongoose = require('mongoose')
+
 app.use(express.static(__dirname)) // hosting a static file from index.html
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+var dbUrl = 'mongodb+srv://cluster0.ukcpx.mongodb.net/<dbname>'
+var Message = mongoose.model('Message' , {
 
-var messages = [
+    name: String,
+    message: String
+})
 
-    {name:'Prakash', message: "Hello"},
-    {name: 'Anamol', message: "Hi"}
-]
+
 app.get('/messages', (req,res) => {
+    Message.find({}, (err, messages)=> {
+
+
+    })
     res.send(messages)
 })
 
 app.post('/messages', (req,res) => {
     console.log(req.body)
+    var message = new Message(req.body)
+
+    messages.save((err) => {
+        if(err){
+            sendStatus(500)
+
+        io.emit('message',req.body)
+        res.sendStatus(200)
+        
+        }
+    })
     messages.push(req.body)
     io.emit('message',req.body)
     res.sendStatus(200)
@@ -33,6 +52,9 @@ io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+    console.log('mongodb connection', err)
+})
 
 var server =http.listen(3000, () => {
     console.log("Server is listening on port", server.address().port)
