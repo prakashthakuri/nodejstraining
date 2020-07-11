ba5859b6baa548508514c1480fe4b73bc5083334
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-07-10 20:22:08 -0400

    Chat App with Node and Socket Connection

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+const { add } = require('lodash')
+const { socket } = require('dgram')
 
 var app = express()
 
-app.use(express.static(__dirname)) // hosting a static file from index.html
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-var server =app.listen(3000, () => {
+app.use(express.static(__dirname)) // hosting a static file from index.html
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+var messages = [
+
+    {name:'Prakash', message: "Hello"},
+    {name: 'Anamol', message: "Hi"}
+]
+app.get('/messages', (req,res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    io.emit('message',req.body)
+    res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+
+var server =http.listen(3000, () => {
     console.log("Server is listening on port", server.address().port)
 })
 
